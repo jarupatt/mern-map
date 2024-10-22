78e066fb44812c442e3fc4b58340fd56e23bfd99
Jarupat <jarupat.cho@student.mahidol.edu>
2024-10-22 21:43:18 +0700

    .

--- src/places/components/PlaceItem.js
+++ src/places/components/PlaceItem.js
@@ -36,7 +36,7 @@ const PlaceItem = (props) => {
     setShowConfirmModal(false);
     try {
       await sendRequest(
-        `http://localhost:5500/api/places/${props.id}`,
+        process.env.REACT_APP_BACKEND_URL + `/places/${props.id}`,
         "DELETE",
         null,
         {
@@ -88,7 +88,7 @@ const PlaceItem = (props) => {
           {isLoading && <LoadingSpinner asOverlay></LoadingSpinner>}
           <div className="place-item__image">
             <img
-              src={`http://localhost:5500/${props.image}`}
+              src={`${process.env.REACT_APP_ASSET_URL}${props.image}`}
               alt={props.title}
             />
           </div>
